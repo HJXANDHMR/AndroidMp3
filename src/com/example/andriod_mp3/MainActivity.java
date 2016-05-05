782b296733dbe505604fed8f6cc6c7325cf550b1
HJXANDHMR <975464817@qq.com>
2016-05-05 18:41:27 +0800

    Update MainActivity.java

--- src/com/example/andriod_mp3/MainActivity.java
+++ src/com/example/andriod_mp3/MainActivity.java
@@ -19,8 +19,6 @@ public class MainActivity extends Activity {
 	private Button bt_again;
                  private Button bt_gittest
                 // 新增加的内容
-               //test
-	//逗比
 	private MediaPlayer mediaPlayer;
 
 	@Override
